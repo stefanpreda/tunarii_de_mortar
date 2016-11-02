90e078fce6d0190406894c775e3d41502a19bdad
stefanpreda <stefan.alexandru.preda@gmail.com>
2016-11-02 16:37:58 +0200

    Working client-server

---
+++ explosive_pacman/Assets/Control/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //This allows default w,a,s,d movement
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
