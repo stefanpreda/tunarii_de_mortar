118635d7be6b083601436ef67c879f7ab916d1d4
stefanpreda <stefan.alexandru.preda@gmail.com>
2016-11-03 21:27:58 +0200

    Animations working but not over network.

--- explosive_pacman/Assets/Control/PlayerController.cs
+++ explosive_pacman/Assets/Control/PlayerController.cs
@@ -3,15 +3,54 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    Animator anim;
+    public float speed = 11.0f;
+
+    void Start()
+    {
+        //anim = NetworkServer.FindLocalObject(netId).GetComponent<Animator>();
+        //anim = GetComponent<NetworkAnimator>();
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        if (!isLocalPlayer)
+        if (!isLocalPlayer || anim == null)
         {
             return;
         }
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("left", true);
+            anim.SetBool("right", false);
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("left", false);
+            anim.SetBool("right", true);
+            anim.SetBool("up", false);
+            anim.SetBool("down", false);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("up", true);
+            anim.SetBool("down", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+            anim.SetBool("up", false);
+            anim.SetBool("down", true);
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
         transform.Translate(x, y, 0);
     }
